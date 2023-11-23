788bba4fc6cff0b5266d8661c296879ca8366007
Evan JOFFRIN <evan.joffrin@interexa.fr>
2023-11-24 00:54:43 +0100

    Initialisation projet

---
+++ snakes/src/main/java/fr/team92/snakes/App.java
@@ -0,0 +1,13 @@
+package fr.team92.snakes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
